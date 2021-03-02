b38b979cc995b2f3c1afc2209d2833cd8f2e2f63
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 13:51:49 -0600

    working on if than else statements

--- src/App.java
+++ src/App.java
@@ -15,7 +15,23 @@ public class App {
        int anotherVar = 50; myVar--; 
        //can be on the same line after a semi-colon 
 
-       
+       boolean gameOver = true; 
+       int score = 4000; 
+       int levelCompleted = 5; 
+       int bonus = 100; 
+
+    //    if (score <  5000 && score > 1000) {
+    //        System.out.println("Your score was less than 5000");
+    //    } else if (score < 1000) {
+    //        System.out.println("less than 1000");
+    //    } else {
+    //         System.out.println("got here");
+    //      }
+
+    if (gameOver == true) {
+        int finalScore = score + (levelCompleted * bonus); 
+        System.out.println("Your final score was " + finalScore);
+    }
 
     }
 }
