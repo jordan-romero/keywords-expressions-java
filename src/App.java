e675a5e4599033013453675421400040d9a53722
jordles113 <jordan.t.romero@gmail.com>
2021-03-02 13:33:38 -0600

    inital commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+       double kilos = (100 * 1.609344); 
+       int highScore = 50; 
+
+       if (highScore == 50) {
+           System.out.println("This is an expression");
+       }
+
+    }
+}
